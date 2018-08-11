156a263ccd09deeaa43be299eddee054c6346d5a
noribang <hoonk9387@gmail.com>
2018-08-10 23:43:04 -0500

    Added js for weather api call.

---
+++ api01a-weathermap/api-vanilla-js/weather-interface.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	$('#weatherLocation').click(function() {
+		const city = $('#location').val();
+		$('#location').val("");
+		// alert(city);
+		let request = new XMLHttpRequest();
+		// Template string for url string value.
+		const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a7524e6c02f6f8db56c4ef87e58f793c`;
+		
+		request.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+				const response = JSON.parse(this.responseText);
+				getElements(response);
+			}
+		}
+
+		request.open("GET", url, true);
+		request.send();
+
+		const getElements = function(response) {
+			$('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+			$('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+		}
+	});
+});
